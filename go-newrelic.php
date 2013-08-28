76dc004be0422eff3092cbc617f118d970b66ac7
Zach Tirrell <zbtirrell@gmail.com>
2013-08-28 08:50:23 -0400

    updating comment header block

--- go-newrelic.php
+++ go-newrelic.php
@@ -1,13 +1,13 @@
 <?php
-/*
-Plugin Name: GigaOM New Relic
-Plugin URI: http://gigaom.com
-Description: Sets the New Relic app name and other configuration values based on WordPress configuration. New Relic profiles code performance and activity, http://newrelic.com .  
-Version: 0a
-Author:  misterbisson
-License: GPL2
-*/
-  
+/**
+ * Plugin Name: Gigaom New Relic
+ * Plugin URI: http://gigaom.com
+ * Description: Sets the New Relic app name and other configuration values based on WordPress configuration. New Relic profiles code performance and activity, http://newrelic.com .
+ * Version: 0a
+ * Author:  misterbisson
+ * License: GPL2
+ */
+
 // include required components
 require_once dirname( __FILE__ ) .'/components/class-go-newrelic.php';
 go_newrelic();
